312b3c59ab0e8c06968f6ae2c322fd99dfe78c00
theonlineaid <nkomorfaruk@gmail.com>
2024-09-29 16:38:00 +0600

    add market filter demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,32 @@
-import AGgRightClick from './components/AGgRightClick'
+// import AGgRightClick from './components/AGgRightClick'
 // import AGLocalSave from './components/AGLocalSave'
-import Example1 from './components/Example1'
-import Example2 from './components/Example2'
-import Example3 from './components/Example3'
-import Example4 from './components/Example4'
-import ExampleWithError from './components/ExampleWithError'
-import ExampleWithRowClass from './components/ExampleWithRowClass'
-import OrderSummary from './components/OrderSummery'
-import TestComponent from './components/TestComponent'
+// import OrderSummary from './components/OrderSummery'
+// import TestComponent from './components/TestComponent'
+// import Example1 from './components/Example1'
+// import Example2 from './components/Example2'
+// import Example3 from './components/Example3'
+// import Example4 from './components/Example4'
+// import ExampleWithError from './components/ExampleWithError'
+// import ExampleWithRowClass from './components/ExampleWithRowClass'
+
+import { AgGridMarketData } from "./ag/AgGridMarketData";
 
 export default function App() {
   return (
-    <div style={{maxWidth: '1000px', margin: 'auto'}}>
+    <div style={{ maxWidth: '1000px', margin: 'auto' }}>
 
-      <TestComponent />
 
-      <OrderSummary />
-      <br />
-      <hr />
-      <br />
-
-      <AGgRightClick />
+      <AgGridMarketData />
+      {/* <TestComponent /> */}
+      {/* <OrderSummary /> */}
+      {/* <AGgRightClick /> */}
       {/* <AGLocalSave /> */}
-      <Example1 />
-
-      <br />
-      <hr />
-      <br />
-
-
-      <Example2 />
-
-      <br />
-      <hr />
-      <br />
-
-      <Example3 />
-
-      <br />
-      <hr />
-      <br />
-
-      <Example4 />
-
-      <br />
-      <hr />
-      <br />
-      <ExampleWithError />
-
-
-      <br />
-      <hr />
-      <br />
-
-      <ExampleWithRowClass />
+      {/* <Example1 /> */}
+      {/* <Example2 /> */}
+      {/* <Example3 /> */}
+      {/* <Example4 /> */}
+      {/* <ExampleWithError /> */}
+      {/* <ExampleWithRowClass />  */}
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
 import "ag-grid-community/styles/ag-theme-quartz.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
+import 'ag-grid-community/styles/ag-theme-balham.css';
+
 import './index.css'
+import { MarketProvider } from "./context/MarketContext.tsx";
+
+const App = React.lazy(() => import(`./App.tsx`))
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <MarketProvider>
+      <Suspense fallback={<div>Loading</div>}>
+        <App />
+      </Suspense>
+    </MarketProvider>
   </React.StrictMode>
 );
